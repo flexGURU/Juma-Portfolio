2a2dadb372a93f05f5d49860ecbd97f32886b0fd
John Nganga <mukunajohn329@gmail.com>
2024-11-29 21:53:45 +0300

    final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { ExtraOptions, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 const extraOptions: ExtraOptions = {
   scrollPositionRestoration: 'enabled',
@@ -10,5 +12,10 @@ const extraOptions: ExtraOptions = {
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot()
+    )
+  ]
 };
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  contactDetails: FormGroup
+
+  constructor(
+    private fb:FormBuilder, 
+    private toastr: ToastrService) 
+
+    {
+    this.contactDetails = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+
+    })
+  }
+
+  get formControls(){
+    return this.contactDetails.controls
+  }
+
+
+
+  async submit(){
+    console.log(this.contactDetails.value);
+
+    emailjs.init('-H-AP4PFvRyThJ50N')
+    let response = await emailjs.send('service_5vq42js', 'template_k1m3qny', {
+      name: this.contactDetails.value.name,
+      email: this.contactDetails.value.email,
+      subject: this.contactDetails.value.subject,
+      message: this.contactDetails.value.message
+    })
+
+    this.contactDetails.reset();
+    this.toastr.success('Email sent succesfully')
+
+  }
+
+  
+
+
+  
+
+
+  
+
 
 }
