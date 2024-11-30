c92d6a3a25930d45c1bc838c5903958943450d6d
John Nganga <mukunajohn329@gmail.com>
2024-11-30 23:36:47 +0300

    final

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  homes = 
+    {
+      content: 
+      `Empowering businesses with seamless, scalable, and secure cloud solutions that drive 
+      innovation and efficiency. Transforming complex challenges into simple, agile solutions for a 
+      smarter, more connected future.`
+    }
+
+    navigate(){
+      window.open('https://www.linkedin.com/in/james-juma-sindani/', "_blank")
+    }
+  
+
 }
+
