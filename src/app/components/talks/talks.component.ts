9e95b6c08bfa069128154499dc5dbc35ffb98859
John Nganga <mukunajohn329@gmail.com>
2024-12-01 00:10:31 +0300

    final

--- src/app/components/talks/talks.component.ts
+++ src/app/components/talks/talks.component.ts
@@ -11,11 +11,29 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 })
 export class TalksComponent {
 
+  tubeVids = [
+    {
+      src: 'https://www.youtube.com/embed/FyYHRPCrpMo',
+      title: 'Cloud Force sky bootcamp'
+    },
+    {
+      src: 'https://www.youtube.com/embed/9st7RVY7kdA',
+      title: 'AWS user group part one'
+    },
+    {
+      src: 'https://www.youtube.com/embed/btA7H5m6sIQ',
+      title: 'AWS usergroup part two'
+    },
+    {
+      src: 'https://www.youtube.com/embed/tyFfmFSAQwQ',
+      title: 'IT Career sessions with Kevin'
+    }
+  ]
+
   imgPathLg = [
     
       '../../../assets/3.jpg',
       '../../../assets/4.jpg',
-      '../../../assets/5.jpg',
       '../../../assets/6.jpg',
       '../../../assets/52.jpg',
       '../../../assets/7.jpg',
