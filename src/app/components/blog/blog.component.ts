d257acd402482fcde3d0cfd34484c7e7a7bf7255
John Nganga <mukunajohn329@gmail.com>
2024-12-01 00:00:28 +0300

    final

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -15,6 +15,7 @@ export class BlogComponent {
       title: 'The Azubi Africa Cloud Wave',
       time: '4 min',
       imgPath: '../../../assets/azubi.jpg',
+      route: 'https://www.linkedin.com/pulse/azubi-africa-cloud-wave-james-juma-sindani/',
       content: 
       `The cloud is a powerful tool that can help you accomplish your goals and missions. 
       But it's not just about the technology—you need to understand how the cloud can fit into your 
@@ -25,6 +26,7 @@ export class BlogComponent {
       title: 'Aws Cloud Roles',
       time: '3 min',
       imgPath: '../../../assets/roles.jpeg',
+      route: 'https://www.linkedin.com/pulse/aws-cloud-roles-james-juma-sindani/',
       content: 
      `The cloud has changed how we work. It's no longer just a place where you can store your files. 
       It's a tool that allows you to create and grow your business, from anywhere in the world. 
@@ -35,10 +37,16 @@ export class BlogComponent {
       title: 'Traditional Monolithic vs 3-Tier Architectures',
       time: '4 min',
       imgPath: '../../../assets/mon.jpeg',
+      route: 'https://www.linkedin.com/pulse/traditional-monolithic-vs-3-tier-architectures-james-juma-sindani/',
       content: 
       `When it comes to building and deploying software, there are a variety of architectural patterns to choose from. 
       One popular option is the 3-tier architecture, which is a variation of the traditional monolithic architecture. In this post, we'll discuss the two architectures, highlighting their pros and cons.`
     }
   ]
+
+  navigate(route){
+    window.open(route, "_blank")
+
+  }
 
 }
--- src/app/components/talks/talks.component.ts
+++ src/app/components/talks/talks.component.ts
@@ -1,14 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-talks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './talks.component.html',
   styleUrl: './talks.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TalksComponent {
+
+  imgPathLg = [
+    
+      '../../../assets/3.jpg',
+      '../../../assets/4.jpg',
+      '../../../assets/5.jpg',
+      '../../../assets/6.jpg',
+      '../../../assets/52.jpg',
+      '../../../assets/7.jpg',
+      '../../../assets/8.jpg'
+    
+  ]
+
+  imgPathSm = [
+     '../../../assets/1.jpg',
+     '../../../assets/2.jpg',
+     '../../../assets/3.jpg',
+    '../../../assets/4.jpg',
+    '../../../assets/5.jpg',
+    '../../../assets/6.jpg',
+    '../../../assets/52.jpg',
+    '../../../assets/7.jpg',
+    '../../../assets/8.jpg'
+  
+]
+
 
  
 
